9a7facf0f3b149f89117ec346641bd4b884fc255
Allen Hai <allenhai03@gmail.com>
2019-10-18 11:16:43 -0500

    🔼 create deployments concurrently w/ Promise.all

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const { createDeployment } = require("now-client");
 
-async function deploy() {
-  console.log("deploying site 1 ...");
-  let deployment;
+let sitePaths = [
+  `${__dirname}/dist/site1`,
+  `${__dirname}/dist/site2`,
+  `${__dirname}/dist/site3`,
+  `${__dirname}/dist/site4`,
+  `${__dirname}/dist/site5`,
+  `${__dirname}/dist/site6`,
+  `${__dirname}/dist/site7`,
+  `${__dirname}/dist/site8`,
+  `${__dirname}/dist/site9`,
+  `${__dirname}/dist/site10`
+];
 
-  for await (const event of createDeployment("dist/site1", {
+Promise.all(
+  sitePaths.map(sitePath =>
+    deploy(sitePath)
+      .then(d => d)
+      .catch(console.error)
+  )
+).then(console.log);
+
+async function deploy(sitePath) {
+  console.log(`${sitePath}`);
+  let deployment;
+  for await (const event of createDeployment(sitePath, {
     token: process.env.NOW_TOKEN
   })) {
     if (event.type === "ready") {
@@ -14,7 +34,3 @@ async function deploy() {
   }
   return deployment;
 }
-
-deploy()
-  .then(console.log)
-  .catch(console.error);
